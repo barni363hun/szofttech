fc4d962cf309ae3c818bb55aebed734f783dbef0
CzupponAndras <141320664+CzupponAndras@users.noreply.github.com>
2023-11-23 19:53:00 +0100

    Update FileManager.java v2

--- classes/file/FileManager.java
+++ classes/file/FileManager.java
@@ -2,10 +2,10 @@ package classes.file;
 import java.util.*;
 import java.io.*;
 
-class FileManager implements FileRead, FileWrite {
-    public LinkedList<String> getUsers(String filename) {
+class FileManager extends FileRead, FileWrite {
+    public String[][] getUsers(String filename) {
         FileRead list = new FileRead(filename);
-        list.getList();
+        list.getTable();
         return list;
     }
 
