9d8294a737525c4e9efc78a1f3b9105e62d56ee0
CzupponAndras <141320664+CzupponAndras@users.noreply.github.com>
2023-11-23 19:43:06 +0100

    getUsers()

---
+++ FileManager.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class FileManager implements FileRead,FileWrite {
+public LinkedList<String> getUsers(String filename){
+FileRead list =new FileRead(filename);
+list.getList();
+return list;
+}
+setUsers(){
+}
+
+}
