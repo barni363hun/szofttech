f4f621395bd3c12ebd50f89f70fcd3a753437004
CzupponAndras <141320664+CzupponAndras@users.noreply.github.com>
2023-11-29 20:43:09 +0100

    javított FileRead

--- classes/file/FileRead.java
+++ classes/file/FileRead.java
@@ -1,22 +1,37 @@
 package classes.file;
+
 import java.io.*;
+
 import java.lang.*;
+
 import java.util.*;
 
 public class FileRead {
-    public static final String token = "[,;]";
-    static final String filePath = "../data/database.";
-    static final String fileExtension = ".csv";
-    LinkedList<String> lineList = new LinkedList<String>();
 
-    public  FileRead(String filename) {
+    public static final String token = "[,;]";
+
+    static final String filePath = "data/database.";
+
+    static final String fileExtension = ".csv";
+
+    LinkedList<String> lineList = new LinkedList<>();
+
+    public FileRead(String filename) {
+
         File data = new File(this.filePath + filename + this.fileExtension);
+
         if (data.exists()) {
+
             // System.out.println("Opening File: "+data.getName());
+
             // System.out.println("Path: "+data.getAbsolutePath());
+
         } else {
+
             System.out.println("File does not exist.");
+
         }
+
         try {
             BufferedReader inFile = new BufferedReader(new FileReader(data));
             String input = null;
@@ -26,61 +41,94 @@ public class FileRead {
             System.out.println(data.getName() + " loaded");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
         }
     }
 
     public int getListSize() {
+
         return lineList.size();
+
     }
 
     public LinkedList<String> getList() {
+
         return lineList;
+
     }
 
     public String getLine(int number) {
+
         return lineList.get(number);
+
     }
 
-    public String[] splitLine(int number, String token) {
-        String[] splittedLine = lineList.get(number).split(token);
+    public String[] splitLine(int number) {
+
+        String[] splittedLine = lineList.get(number).split(this.token);
+
         return splittedLine;
+
     }
 
     public int splittedLineSize(String[] splittedLine) {
+
         return splittedLine.length;
+
     }
 
-    public int getMaxElements(String token) {
+    public int getMaxElements() {
+
         int max_elements = 0;
+
         for (int i = 0; i < this.getListSize(); i++) {
-            if (max_elements < this.splitLine(i, token).length) {
-                max_elements = this.splitLine(i, token).length;
+
+            if (max_elements < this.splitLine(i).length) {
+
+                max_elements = this.splitLine(i).length;
+
             }
         }
         return max_elements;
+
     }
 
-    public String[][] getTable(String token) {
-        String[][] table = new String[this.getListSize()][this.getMaxElements(token)];
+    public String[][] getTable() {
+        String[][] table = new String[this.getListSize()][this.getMaxElements()];
+
         String[] current_line;
+
         int elements_count;
+
         for (int i = 0; i < this.getListSize(); i++) {
-            current_line = this.splitLine(i, token);
+
+            current_line = this.splitLine(i);
+
             elements_count = current_line.length;
+
             for (int j = 0; j < current_line.length; j++) {
+
                 table[i][j] = current_line[j];
+
             }
+
         }
+
         return table;
+
     }
 
-    public String[] getRow(String token, int r) {
+    public String[] getRow(int r) {
+
         String[] row = new String[this.getListSize()];
+
         for (int i = 0; i < this.getListSize(); i++) {
-            row[i] = this.splitLine(i, token)[r];
+
+            row[i] = this.splitLine(i)[r];
+
         }
+
         return row;
+
     }
 
 }
